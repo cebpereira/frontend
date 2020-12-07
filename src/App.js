83e4887a91888ea6359056b292344a476eda84aa
Carlos Elandro <carlos.wx290@gmail.com>
2020-12-06 23:40:54 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {useState} from 'react';
+
+function App() {
+
+  const [counter,setCounter] = useState(0);
+  function increment() {
+    setCounter(counter + 1);
+    console.log(counter)
+  }
+  return (
+    <div className="App">
+      <div>
+        contador:{counter}
+      </div>
+      <button onClick={increment}>Incremento</button>
+    </div>
+  );
+}
+
+export default App;
